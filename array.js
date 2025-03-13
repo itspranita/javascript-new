f68dafb4999f288208c778622ad5b0a386382be4
itspranita <mpranita25@gmail.com>
2025-03-13 15:24:47 +0530

    update content

---
+++ array.js
@@ -0,0 +1,25 @@
+let ar=[12,34.2,"cybrom",true]
+// console.log(ar[2])
+             
+
+
+
+            // for of
+for(let vl of ar) {
+    console.log(vl)
+}           //returns element
+    
+
+            // for in
+  for(let k in ar){
+    console.log(k)  
+  }
+       //returns indexing value
+
+
+
+       let ar =[]
+       let a=parseInt(prompt("enter five elements "))
+       document.
+
+
---
+++ eventlistner.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector('#btn')
+
+
+// btn.addEventListner('click',()=>{alert("fasfd")})
+
+
+
+let btn1 = document.querySelector('#btn1')
+
+
+btn1.addEventListner('dblclick',()=>{alert("hello")})
